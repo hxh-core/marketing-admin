6136f45a8ad2c15387f84cfe9b8235df9f48d9c1
ker4ik13 <kekustop228@mail.ru>
2024-08-14 11:26:20 +0300

    Added protected populate package

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,4 +2,10 @@ export default ({ env }) => ({
   "soft-delete": {
     enabled: true,
   },
+  "protected-populate": {
+    enabled: true,
+    config: {
+      ["auto-populate"]: true,
+    },
+  },
 });
